271b8a41b59927e82b387ce98176d0fc2a148c72
Anton Johansson <anton.xdd@gmail.com>
2017-12-20 19:55:00 +0100

    Added visitor log with time and request print

--- app.js
+++ app.js
@@ -26,6 +26,56 @@ updateWeek();
 
 
 app.get('/', function (req, res) {
-  console.log("called for week", week);
+    
+    console.log("req",req);
+    
+    
+    
+    // Connection URL
+    const dbName = "Vintergatan5a-analystics";
+    var url = 'mongodb://localhost:27017/Vintergatan5a-analystics';
+
+    
+    // Add visitor statistics
+    var insertDocuments = function(db, callback) {
+          // Get the documents collection
+          var collection = db.collection('visitors');
+          // Insert some documents
+          let d = new Date();
+          
+          collection.insertMany([{date: d.getTime()}], function(err, result) {
+            assert.equal(err, null);
+            //console.log("Inserted "+result.ops.length+" documents into the document collection");
+            callback(result);
+          });
+    }
+
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function(err, database) {
+      assert.equal(null, err);
+      console.log("Connected correctly to server");
+        
+        const myDB = database.db(dbName);
+        const collection = myDB.collection('visitors');
+        
+        findDocuments(myDB, function() {
+            //console.log("Found data!");
+        });
+        
+        //insertDocuments(myDB, function() {
+            //console.log("Inserted data!");
+        //});
+         
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   console.log("called for week", week);
    res.send("Vecka: " + week);
 })
